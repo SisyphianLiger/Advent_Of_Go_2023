d79e1e2c40cbd8f21bb57b9f2e55f94eddb69622
SisyphianLiger <69813075+SisyphianLiger@users.noreply.github.com>
2023-12-25 23:57:01 +0100

    Cannot figure out why this is not a solution

--- Day_1_Trebuchet/trebuchet.go
+++ Day_1_Trebuchet/trebuchet.go
@@ -2,30 +2,98 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 	"unicode"
 )
 
-func storeInput(input os.File) [][]rune { 
 
-    scanner := bufio.NewScanner(&input)
+func outputPrinter(output [][]string) {
+    for i, innerSlice := range output {
+        fmt.Printf("Slice %d: ", i + 1)
+        for _, number := range innerSlice {
+            fmt.Printf("%s ", number)
+        }
+        fmt.Println()
+    }
+}
+
+
+func findFirstNum(input string, m map[string]string) string {
+    word := "" 
+    for i := 0; i < len(input); i++ {
+       
+        if unicode.IsNumber(rune(input[i])) {
+            return string(input[i])
+        } 
+        
+        word += string(input[i])
+
+        if testDigitPrefix(m, word) {
+            _, ok := m[word]
+            if !ok {
+                continue 
+            }
+            break
+        }
+        word = string(input[i])
+    } 
+    return m[word]
+}
+
+func findLastNum(input string, m map[string]string) string {
+    word := "" 
+    for i := len(input) - 1; i > -1; i-- {         
+        if unicode.IsNumber(rune(input[i])) {
+            return string(input[i])
+        } 
+        
+        word = string(input[i]) + word
+
+        if testDigitSuffix(m, word) {
+            _, ok := m[word]
+            if !ok {
+                continue 
+            }
+            break
+        }
+        word = string(input[i])
+    } 
+    return m[word]
+}
+
+func storeInput(input *os.File) [][]string { 
+
+    scanner := bufio.NewScanner(input)
     scanner.Split(bufio.ScanLines)
     
-    output := [][]rune{}
-
-    for scanner.Scan() {
-        var lines = []rune{}
-       
-        for _,ele := range scanner.Text() {
-            if unicode.IsNumber(ele) {
-                lines = append(lines, ele)
-            }
-        }
-        output = append(output, lines)
+    var numberMap = map[string]string {
+        "one": "1",
+        "two": "2", 
+        "three": "3",
+        "four": "4",
+        "five": "5",
+        "six": "6", 
+        "seven": "7",
+        "eight": "8", 
+        "nine": "9", 
     }
 
-    
+
+    output := [][]string{}
+
+    for scanner.Scan() {
+        var lines = []string{}
+        lines = append(lines, findFirstNum(scanner.Text(), numberMap))
+        lines = append(lines,findLastNum(scanner.Text(), numberMap))
+        output = append(output, lines)
+    }
+ 
+    // outputPrinter(output)
+
     if err := scanner.Err(); err != nil {
         log.Fatal(err)
     }
@@ -33,37 +101,56 @@ func storeInput(input os.File) [][]rune {
     return output
 }
 
-/*
+func testDigitPrefix(m map[string]string, s string) bool {
+    for str,_ := range m {
+        if strings.HasPrefix(str, s) {
+            return true
+        }
+    }
+    return false
+}
 
-The newly-improved calibration document consists of 
-lines of text; each line originally contained a specific 
-calibration value that the Elves now need to recover.
-On each line, the calibration value can be found by 
-combining the first digit and the last digit (in that order)
-to form a single two-digit number.
+func testDigitSuffix(m map[string]string, s string) bool {
+    for str,_ := range m {
+        if strings.HasSuffix(str, s) {
+            return true
+        }
+    }
+    return false
+}
 
-*/
-func calibrateOutput(output [][]rune) int {
+func calibrateOutput(output [][]string) int {
+    sum := 0
+    for _, chrs := range output {
 
-    return 0
+        fstr := chrs[0]
+        sstr := chrs[len(chrs) - 1]
+        combo, err := strconv.Atoi(fstr + sstr)
+
+        if err != nil {
+            log.Fatal(err)
+        }
+               
+        sum += combo
+
+    }
+
+    return sum
 }
 
 
 func main() {
-    input := "testinput.txt"
-    file, err := os.Open(input)
+    args := os.Args[1]
 
+    file, err := os.Open(args)
     if err != nil {
+        fmt.Printf("Invalid file descriptor: please check spelling\n")
         log.Fatal(err)
     }
 
     defer file.Close()
 
-    // Start here
-    output := storeInput(*file)
-    calibrateOutput(output)
-
-
-    
-    
+    // Start here --- yes this is ugly lol
+    fmt.Println("The value the Nisse Need:", calibrateOutput(storeInput(file)))
+ 
 }
